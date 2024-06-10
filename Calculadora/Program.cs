5c0d8517b9d8b94a7ca9ada2652c7d0befe529cd
NataPires <pires.nata1998@gmail.com>
2024-06-10 06:48:56 -0300

    adicionando as classe de subtração, multiplicação e divisão

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -2,7 +2,18 @@
 
 CalculadoraImp c = new CalculadoraImp();
 
-int num1 = 5;
-int num2 = 10;
+int num1 = 10;
+int num2 = 7;
 
 Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
+Console.WriteLine($"{num1} - {num2} = {c.Subtrair(num1, num2)}");
+Console.WriteLine($"{num1} * {num2} = {c.Multiplicar(num1, num2)}");
+
+try
+{
+    Console.WriteLine($"{num1} / {num2} = {c.Dividir(num1, num2)}");
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine("Erro: " + ex.Message);
+}
--- Calculadora/Services/CalculadoraImp.cs
+++ Calculadora/Services/CalculadoraImp.cs
@@ -12,6 +12,25 @@ namespace Calculadora.Services
             return num1 + num2;
         }
 
+        public int Subtrair(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Dividir(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("O divisor não pode ser zero.");
+            }
+            return num1 / num2;
+        }
+
         public bool EhPar(int num)
         {
             return num % 2 == 0;
